06bffc2afd65511620e50dd56d93a386504b559d
venkataramireddyyarram <s554991@nwmissouri.edu>
2022-10-26 18:52:38 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var times= 0;
-
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  times=times+1;
-  res.send(`User accesses are: ${times}`);
+  res.render('mydata', { title: 'Venkata Rami Reddy Yarram' });
 });
 
 module.exports = router;
